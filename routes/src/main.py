225f27e6585211434524ee83cea65cb1c89e2d51
Marco Zuliani <m.zuliani@uniandes.edu.co>
2023-11-16 00:12:53 -0500

    Enable Cors

--- routes/src/main.py
+++ routes/src/main.py
@@ -2,9 +2,18 @@ from fastapi import FastAPI
 from src.routers import route
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import Response
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Enable CORS for all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Set this to the appropriate list of allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request, exc):
